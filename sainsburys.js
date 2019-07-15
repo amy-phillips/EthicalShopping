4dedc8ad15556cc4fa04295352dec67c01448df6
amy-phillips <amy@islovely.co.uk>
2019-07-15 22:29:25 +0100

    sainsburys - colour the "before you go" items

--- sainsburys.js
+++ sainsburys.js
@@ -24,6 +24,23 @@ function colour_page(response) {
         }
     });
 
+    // "before you go" (as checking out)
+    document.querySelectorAll('.productNameAndPromotions').forEach( function( product_div ){
+        // has it already been coloured?
+        if(!product_div.parentNode.querySelector('#es-moar-infos')) {
+            // find the product div
+            var tile_content=product_div.parentNode;
+            while(tile_content && !tile_content.classList.contains("productESpot")) {
+                tile_content=tile_content.parentNode;
+            }
+            if(!tile_content) {
+                console.log("Error: failed to find tile_content node for "+product_div.textContent.trim());
+                tile_content=product_div.parentNode;
+            }
+            colour_product(munged_tables, product_div, tile_content, "es-sainsbury-search-result", true, product_div.querySelector("a").textContent.trim());
+        }
+    });
+
     //viewing single product
     document.querySelectorAll('.productTitleDescriptionContainer h1').forEach( function( product_div ){
         if(!product_div.parentNode.querySelector('#es-moar-infos')) {
